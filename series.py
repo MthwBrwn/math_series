6196ca21274cfcdf32ffc76892ca9b119a0b5d58
mthwbrwn <mthwbrwn@gmail.com>
2018-11-27 22:12:59 -0800

    test conditions made for various conditions for fibonacci

--- series.py
+++ series.py
@@ -1,1 +1,19 @@
-def fibonacci ( number ):
+def fibonacci (n):
+    subtotal1, subtotal2 = 0 , 1
+    if n<= 0 :
+        return("you'll need to enter a positive integer")
+    if n == 1:
+        return (subtotal1)
+    for i in range(n - 2):
+        subtotal1, subtotal2 = subtotal2, subtotal1 + subtotal2
+    return(subtotal2)
+
+
+
+
+
+
+# def lucas ():
+#     pass
+
+# if __name__ == "__main__"
--- test_series.py
+++ test_series.py
@@ -1,3 +1,32 @@
-from math_series import fibonacci
+from math_series import series
+# from math_series import fibonacci
+
+def test_assert_true():
+    """ using this to check test working fine
+    """
+    assert True is True
+
+""" test for the third figure in series 0,1,1"""
+def test_fibonacci_at3():
+    actual = 3
+    expected = 1
+    assert series.fibonacci(actual) == expected
+
+"""" test for the first position when output should be 0"""
+def test_fibonacci_at0():
+    actual = 1
+    expected = 0
+    assert series.fibonacci(actual) == expected
+
+""" test for answer at a large number"""
+def test_fibonacci_at13():
+    actual = 13
+    expected = 144
+    assert series.fibonacci(actual) == expected
+
+def test_fibonacci_as_string():
+    actual = str
+    expected = 144
+    assert series.fibonacci(actual) == expected
 
 
