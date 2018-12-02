9b3c8e706f6fd8098a8e9df0410c87af2fa7fe8a
mthwbrwn <mthwbrwn@gmail.com>
2018-12-02 13:44:48 -0800

    linter errors on series.py fixed

--- series.py
+++ series.py
@@ -1,6 +1,7 @@
-def fibonacci (n):
-    subtotal1, subtotal2 = 0 , 1
-    if n<= 0 :
+def fibonacci(n):
+    """ """
+    subtotal1, subtotal2 = 0, 1
+    if n <= 0:
         return("you'll need to enter a positive integer")
     if n == 1:
         return (subtotal1)
@@ -8,11 +9,12 @@ def fibonacci (n):
         subtotal1, subtotal2 = subtotal2, subtotal1 + subtotal2
     return(subtotal2)
 
-""" This is a one argument function that returns a lucas number ()
-"""
-def lucas (n):
-    subtotal1, subtotal2 = 2 , 1
-    if n<= 0 :
+
+def lucas(n):
+    """ This is a one argument function that returns a lucas number ()
+    """
+    subtotal1, subtotal2 = 2, 1
+    if n <= 0:
         return("you'll need to enter a positive integer")
     if n == 1:
         return (subtotal1)
@@ -20,13 +22,14 @@ def lucas (n):
         subtotal1, subtotal2 = subtotal2, subtotal1 + subtotal2
     return(subtotal2)
 
-"""this function uses optional inputs for the user to set up their own number sequences"""
-def sum_series (n , opt1 = 0, opt2 =1):
-    if n<= 0 :
+
+def sum_series(n, opt1=0, opt2=1):
+    """this function uses optional inputs for the user to set up their own number sequences"""
+    if n <= 0:
         return("you'll need to enter a positive integer")
-    if opt1< 0 :
+    if opt1 < 0:
         return("you'll need to enter a positive integer")
-    if opt2< 0 :
+    if opt2 < 0:
         return("you'll need to enter a positive integer")
     subtotal1, subtotal2 = opt1, opt2
     if n == 1:
